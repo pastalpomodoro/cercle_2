0039ac25948d019bce336addb5ce60c2bb07757e
root <root@DESKTOP-UD26204.localdomain>
2024-12-16 20:11:09 +0100

    salut

--- push_swap/nm_pile_a.c
+++ push_swap/nm_pile_a.c
@@ -1,28 +1,36 @@
 #include "push_swap.h"
 
+int total_moove(int index, int num, t_num **pile_a, t_num **pile_b)
+{
+    if (index > my_lstlen(pile_a) / 2)
+        index = -(my_lstlen(pile_a) - index);
+    ft_printf("NUM: %d   INDEX: %d   P_B: %d\n", num, index, nm_pile_b(num, pile_b));
+    return (index + nm_pile_b(num, pile_b));
+}
 int nm_pile_a(t_num **pile_a, t_num **pile_b)
 {
     int i;
+    int num;
     int count;
     int temp_count;
     t_num *temp;
 
     temp = *pile_a;
-    count = nm_pile_b(temp->num, pile_b);
-    i = 1;
-    ft_printf("PILE_B MOOVE: %d\n", count);
+    count = total_moove(0, temp->num, pile_a, pile_b);
     temp = temp->next;
+    i = 1;
+    num = temp->num;
     while (temp != NULL)
     {
-        temp_count = nm_pile_b(temp->num, pile_b);
-        if (i > my_lstlen(pile_a) / 2)
-            i = my_lstlen(pile_a) - i + 1;
-        ft_printf("INDEX: %d   NUM: %d\n", i, temp->num);
-        temp_count += i;
-        if (count > temp_count)
+        temp_count = total_moove(i, temp->num, pile_a, pile_b);
+        if (count > absolue(temp_count))
+        {
             count = temp_count;
+            num = temp->num;
+        }
         temp = temp->next;
         i++;
     }
+    ft_printf("\nNUM: %d   COUNT: %d", num, count);
     return (count);
 }
